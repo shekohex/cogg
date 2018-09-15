9389dcefb8b3f1ccd803a06770a9dba4f9ae95b5
Shady Khalifa <shekohex@gmail.com>
2018-09-15 07:12:57 +0200

    make service worker

--- cogg/src/server/state.rs
+++ cogg/src/server/state.rs
@@ -1,7 +1,17 @@
 use chrono::prelude::*;
-use log::debug;
+use colored::Colorize;
+use lazy_static::lazy_static;
+use log::{debug, error};
 use protos::users::User;
 use std::collections::HashMap;
+use std::sync::Mutex;
+use std::time::{Duration, Instant};
+use tokio::prelude::*;
+use tokio::timer::Interval;
+lazy_static! {
+    pub(crate) static ref SERVER_STATE: Mutex<ServerState> = Mutex::new(ServerState::new());
+}
+
 #[derive(Debug)]
 pub(crate) struct ServerState {
     /// Holds Online Users
@@ -31,11 +41,62 @@ impl ServerState {
     pub fn ping_user(&mut self, username: &str) -> i64 {
         let now = Utc::now();
         let ts = now.timestamp_millis();
-        if self.users.get(username).is_some() {
+        if self.users.contains_key(username) {
             self.users.insert(username.to_string(), ts);
             return ts;
         } else {
             return 0;
         }
+    }
+}
+
+pub(crate) struct ServiceWorker {
+    check_ervery: u64,
+    is_working: bool,
+}
+
+impl ServiceWorker {
+    pub fn new(check_ervery: u64) -> Self {
+        ServiceWorker {
+            check_ervery,
+            is_working: false,
+        }
+    }
+
+    pub fn work(&mut self) {
+        if self.is_working || self.check_ervery == 0 {
+            return;
+        }
+        let runner = |_instant| {
+            let mut removed: Vec<String> = Vec::new();
+            let lock = SERVER_STATE.try_lock();
+            if let Ok(mut state) = lock {
+                let now = Utc::now();
+                let now_ts = now.timestamp_millis();
+                state.users.retain(|username, &mut ts| {
+                    let last_ping = Duration::from_millis(ts as u64);
+                    let now_ms = Duration::from_millis(now_ts as u64);
+                    let diff = now_ms - last_ping;
+                    if diff.as_secs() > 5 {
+                        removed.push(username.to_string());
+                        return false;
+                    }
+                    true
+                });
+                if !removed.is_empty() {
+                    debug!("Removed {} Users: {:?}", removed.len(), removed);
+                }
+                debug!("We Have {} Users Connected", state.users.len());
+            } else {
+                error!("Error While trying to get server state, it may block!");
+            }
+            Ok(())
+        };
+        let task = Interval::new(Instant::now(), Duration::from_millis(self.check_ervery))
+            .for_each(runner)
+            .map_err(|e| error!("Failed While Running Worker; err={:?}", e));
+        self.is_working = true;
+        debug!("{}", "Worker Started OK!".green());
+        tokio::run(task);
     }
 }
