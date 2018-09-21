f1348cb43277bc9518d5f26afeca6b2732122d60
Shady Khalifa <shekohex@gmail.com>
2018-09-21 05:32:00 +0200

    detect changes to other proto files

--- cogg/build.rs
+++ cogg/build.rs
@@ -4,8 +4,10 @@ fn main() {
     let proto_root = "src/protos";
     let proto_users = "src/protos/cogg/files.proto";
     let proto_files = "src/protos/cogg/users.proto";
+    let proto_processes = "src/protos/cogg/processes.proto";
     println!("cargo:rerun-if-changed={}", proto_files);
     println!("cargo:rerun-if-changed={}", proto_users);
+    println!("cargo:rerun-if-changed={}", proto_processes);
     protoc_grpcio::compile_grpc_protos(
         &["cogg/files.proto", "cogg/users.proto", "cogg/processes.proto"],
         &[proto_root],
